e127489a85c58b83803c51ab147a47418e89d0cf
Usman X <usmanpervez5@gmail.com>
2024-10-15 22:53:06 +0500

    Initial Commit

---
+++ React_Router/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Home/Home'
+import Home from './components/Footer/Footer'
+import Footer from './components/Header/Header'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
